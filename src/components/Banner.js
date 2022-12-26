a7aa8695b0cea46fa294b64ededd9a117859645a
huydev0401 <quanghuynguyennqh@gmail.com>
2022-12-26 08:19:29 +0700

    Update banner mobile

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,14 +3,18 @@ import React from "react";
 export default function Banner() {
   return (
     <section className="bg-[url('../public/images/banner.png')] bg-center bg-no-repeat bg-cover h-[100vh] md:pt-[100px] pt-[100px]">
-      <div className="page-container max-w-[670px] flex flex-col items-center justify-center">
-        <p className="text-[20px] md:text-[30px] font-medium uppercase text-center text-white leading-[1]">
+      <div className="flex flex-col items-start justify-center page-container">
+        <p className="md:mt-[20px] text-[20px] md:text-[30px] font-medium uppercase text-center text-white leading-[1]">
           hải trình
         </p>
         <h2>
-          <img srcSet="/images/heading.png 2x" alt="" />
+          <img
+            className="h-[100px] md:h-auto"
+            srcSet="/images/heading.png 2x"
+            alt=""
+          />
         </h2>
-        <h3 className="text-center text-white tracking-[1px] mb-[10px]">
+        <h3 className="text-white tracking-[1px] mb-[10px]">
           <span className="font-light text-[14px] md:text-[24px] italic block">
             Trải nghiệm nghỉ dưỡng sang trọng
           </span>
@@ -18,7 +22,7 @@ export default function Banner() {
             Siêu du thuyền hiện đại nhất thế giới
           </span>
         </h3>
-        <h3 className="font-bold text-[32px] md:text-[64px] text-white tracking-[1px] mb-[20px] md:mb-[40px] leading-[1]">
+        <h3 className="font-bold text-[32px] md:text-[64px] text-white tracking-[1px] mb-[10px] md:mb-[20px] leading-[1]">
           SPECTRUM{" "}
           <span className="italic text-[18px] md:text-[36px]">OF THE</span> SEAS
         </h3>
@@ -37,16 +41,16 @@ export default function Banner() {
               <div className="content bg-purple">Phuket</div>
             </div>
           </div>
-          <div className="tracking-[1px]">
-            <p className="md:text-left text-center text-[16px] md:text-[24px] text-white">
+          <div className="tracking-[1px] self-start">
+            <p className="text-left md:text-center text-[16px] md:text-[24px] text-white">
               Dự kiến khởi hành
             </p>
-            <p className="md:text-left text-center text-[32px] md:text-[40px] font-bold text-[#78FFFF] leading-[1]">
+            <p className="text-left md:text-center text-[32px] md:text-[40px] font-bold text-[#78FFFF] leading-[1]">
               30/03/2023
             </p>
           </div>
         </div>
-        <div className="relative btn-banner">
+        <div className="relative btn-banner ml-[15px]">
           <div className="circle absolute top-[-16px] left-[-16px] w-[56px] h-[56px] bg-[#0791BE] rounded-full"></div>
           <a
             className="relative z-[5] inline-flex items-center gap-[14px]"
